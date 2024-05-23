15aa7a08f06a824cefef269d9caa4952028488c0
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 22:11:40 +0500

    first commit

---
+++ app.ts
@@ -0,0 +1,12 @@
+
+//Q:12 Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s
+//name, print a message to them. The text of each message should be the same, but each message should be 
+//personalized with the person’s name.
+
+let Names:string [] = ["ahmed","salman","asif","muskan","saeed"];
+
+console.log(`wish you all the best, ${Names[0]}`);
+console.log(`wish you all the best, ${Names[1]}`);
+console.log(`wish you all the best, ${Names[2]}`);
+console.log(`wish you all the best, ${Names[3]}`);
+console.log(`wish you all the best, ${Names[4]}`);
